2dc9b0d1d047b54f81c7e6c005be7a540b57d7ba
Avani011 <avaniagarwal110@gmail.com>
2024-11-29 02:17:13 +0530

    Error resolve

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // This will configure Next.js to use static export
-  basePath: '/portfolio-avani', // Replace this with your repository name
-  assetPrefix: '/portfolio-avani', // This ensures assets are correctly linked in GitHub Pages
-
-  // Optionally you could add more settings like trailingSlash for correct URLs
-  trailingSlash: true,
 };
 
 export default nextConfig;
