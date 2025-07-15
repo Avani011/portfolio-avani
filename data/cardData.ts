242c2c48da12cd3217b9e9376fd4a2f5f3eb0acd
Avani Agarwal <102690647+Avani011@users.noreply.github.com>
2025-07-16 00:10:41 +0530

    Implement code changes to enhance functionality and improve performance

--- data/cardData.ts
+++ data/cardData.ts
@@ -1,36 +1,42 @@
 import { cardProps } from "@/types";
 
 const cardData: cardProps[] = [
-    {
-        source: "/pr1.svg",
-        name: "HACK IT SAPIENS WEBSITE",
-        size: "small",
-        link: "https://hackitsapiens-2.vercel.app/"
-    },
-    {
-        source: "/pr5.svg",
-        name: "POORNIMA COLLEGE ALUMNI SOCIETY WEBSITE",
-        size: "large",
-        link: "https://pcas.poornima.org/"
-    },
-    {
-        source: "/pr2.svg",
-        name: "FIG PRO",
-        size: "large",
-        link: "https://github.com/Avani011/figma-clone"
-    },
-    {
-        source: "/pr3.svg",
-        name: "PROMPTOPIA",
-        size: "small",
-        link: "https://promptopia-six-chi-24.vercel.app/"
-    },
-    {
-        source: "/pr4.svg",
-        name: "YOUTUBE CLONE",
-        size: "large",
-        link: "https://youtube-clone-eight-swart.vercel.app/"
-    }
+  {
+    source: "/pr1.svg",
+    name: "HACK IT SAPIENS WEBSITE",
+    size: "small",
+    link: "https://hackitsapiens-2.vercel.app/",
+  },
+  {
+    source: "/pr5.svg",
+    name: "POORNIMA COLLEGE ALUMNI SOCIETY WEBSITE",
+    size: "large",
+    link: "https://pcas.poornima.org/",
+  },
+  {
+    source: "/pr6.svg",
+    name: "SYNCBEE",
+    size: "small",
+    link: "https://github.com/Avani011/syncbee",
+  },
+  {
+    source: "/pr2.svg",
+    name: "FIG PRO",
+    size: "large",
+    link: "https://github.com/Avani011/figma-clone",
+  },
+  {
+    source: "/pr3.svg",
+    name: "PROMPTOPIA",
+    size: "small",
+    link: "https://promptopia-six-chi-24.vercel.app/",
+  },
+  {
+    source: "/pr4.svg",
+    name: "YOUTUBE CLONE",
+    size: "large",
+    link: "https://youtube-clone-eight-swart.vercel.app/",
+  },
 ];
 
 export default cardData;
